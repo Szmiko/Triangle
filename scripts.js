69b5706601043236190bb765f9a1541b9cf1b767
Szymon Grabowski <szmiko@wp.pl>
2018-03-04 15:42:14 +0100

    Add prompt

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var a = prompt ('Podaj długość podstawy trójkąta (a)'),
+	h = prompt ('Podaj wysokość trójkąta (h)'),
+	triangleArea = a*h/2;
+
+alert('Pole trójkąta wynosi:' + triangleArea)
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
+
+
+/*var name = prompt('Jaki dzisiaj mamy dzień?');
+alert('Dzisiaj jest ' + name);
+console.log('Dzisiaj jest ' + name);*/
