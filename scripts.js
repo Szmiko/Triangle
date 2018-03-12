1a4be102f8e2562b231c3348d43ad8f9199bdec5
Szymon Grabowski <szmiko@wp.pl>
2018-03-12 16:58:04 +0100

    Fixed files

--- scripts.js
+++ scripts.js
@@ -4,10 +4,3 @@ var a = prompt ('Podaj długość podstawy trójkąta (a)'),
 
 alert('Pole trójkąta wynosi:' + triangleArea)
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-
-
-
-
-/*var name = prompt('Jaki dzisiaj mamy dzień?');
-alert('Dzisiaj jest ' + name);
-console.log('Dzisiaj jest ' + name);*/
